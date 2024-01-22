52061dccff2dec5639f477960b9e162e4c7954c3
fabianNav <fn39001@gmail.com>
2024-01-22 18:58:11 -0300

    Terminamos de hacer la logica del elemento SUMA, ahora proceguimos con la resta

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 
+
+
+
+
+let igual = document.getElementById('sumar');
+let resultadoSuma = document.getElementById('resultadoSuma');
+let operadoresSuma = document.getElementsByClassName('operadorSuma').value;
+
+function sumar(a, b) {
+    return a + b;
+}
+//console.log(sumar(2, 5));
+igual.addEventListener('click', () => {
+    let aSuma = parseInt(document.getElementById('sumaA').value);
+    let bSuma = parseInt(document.getElementById('sumaB').value);
+    resultadoSuma.innerHTML = `${JSON.stringify(aSuma + bSuma)}`;
+})
