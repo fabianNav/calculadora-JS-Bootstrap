2b056b812e60c6bcdbc696dfca5ab20d4cccf884
fabianNav <fn39001@gmail.com>
2024-01-22 19:34:35 -0300

    Terminamos la logica de la resta y seguimos con la multiplicacion

--- main.js
+++ main.js
@@ -2,28 +2,20 @@
 //TODO SUMA
 let igual = document.getElementById('sumar');
 let resultadoSuma = document.getElementById('resultadoSuma');
-
-
-function sumar(a, b) {
-    return a + b;
-}
-//console.log(sumar(2, 5));
 igual.addEventListener('click', () => {
-    let aSuma = parseInt(document.getElementById('sumaA').value);
-    let bSuma = parseInt(document.getElementById('sumaB').value);
+    let aSuma = parseFloat(document.getElementById('sumaA').value);
+    let bSuma = parseFloat(document.getElementById('sumaB').value);
     resultadoSuma.innerHTML = `${JSON.stringify(aSuma + bSuma)}`;
 })
 
 //TODO RESTA
-
-
-
-
-
-
-
-
-
+let igualResta = document.getElementById('restar');
+let resultadoResta = document.getElementById('resultadoResta');
+igualResta.addEventListener('click', () => {
+    let aResta = parseFloat(document.getElementById('restaA').value);
+    let bResta = parseFloat(document.getElementById('restaB').value);
+    resultadoResta.innerHTML = `${JSON.stringify(aResta - bResta)}`;
+})
 
 //TODO MULTIPLICACION
 
