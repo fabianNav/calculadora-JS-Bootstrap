d082281a5fe7e4ad3c7acfc5088e8c76ee442965
fabianNav <fn39001@gmail.com>
2024-01-23 18:17:46 -0300

    Agregamos el modo oscuro y su logica

--- main.js
+++ main.js
@@ -32,5 +32,21 @@ let resultadoDivi = document.getElementById('resultadoDivi');
 igualDivi.addEventListener('click', () => {
     let aDivi = parseFloat(document.getElementById('diviA').value);
     let bDivi = parseFloat(document.getElementById('diviB').value);
-    resultadoDivi.innerHTML = `${JSON.stringify(aDivi * bDivi)}`;
+    resultadoDivi.innerHTML = `${JSON.stringify(aDivi / bDivi)}`;
 })
+
+//TODO MODO OSCURO
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';   //! window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'; Sirve para que el navegador elija que modo establecer segun que modo tenemos activado en el dispositivo(modo noche o modo dia)
+const slider = document.getElementById('slider');
+const setTheme = (tema) => {
+    document.documentElement.setAttribute('data-theme', tema);   //! documentElement es la parte raiz del documento. setAttribute agrega/setea un atributo al elemento especificado. 
+    localStorage.setItem('theme', tema);
+}
+slider.addEventListener('click', () => {
+                        //obtenemos de lS el theme y si el theme es igual a dark entonces pasamos a usar light y viceversa
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+})
+setTheme(localStorage.getItem('theme') || preferedColorScheme);
+
+
